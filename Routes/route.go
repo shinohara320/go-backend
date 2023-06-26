15361ca6558f53ec9dda9998e73e559e95e2558e
shinohara320 <daudmanuwu@gmail.com>
2023-06-26 12:45:42 +0900

    adding login api

--- Routes/route.go
+++ Routes/route.go
@@ -7,4 +7,5 @@ import (
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
+	app.Post("/api/login", controller.Login)
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shinohara320/travel-agent/util"
+)
+
+func isAuthenticate(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	if _, err := util.ParseJwt(cookie); err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+	return c.Next()
+}
