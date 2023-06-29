a2a7540a8e9e3c7113355618a6690994fb732f8b
shinohara320 <daudmanuwu@gmail.com>
2023-06-29 21:57:09 +0900

    fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,11 +2,9 @@ package middleware
 
 import "github.com/gofiber/fiber/v2"
 
-// CorsMiddleware adalah middleware CORS untuk mengatur akses CORS pada server
 func CorsMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		if c.Method() == "OPTIONS" {
-			// Tangani permintaan Preflight
 			c.Set("Access-Control-Allow-Origin", "http://localhost:3000")
 			c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 			c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
@@ -14,7 +12,11 @@ func CorsMiddleware() fiber.Handler {
 			return c.SendStatus(fiber.StatusOK)
 		}
 
-		// Lanjutkan ke middleware berikutnya
+		c.Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		c.Set("Access-Control-Allow-Credentials", "true")
+
 		return c.Next()
 	}
 }
