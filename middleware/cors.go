0725c4d3ec81b056a7c3fe5b29bc939d8de0e740
shinohara320 <daudmanuwu@gmail.com>
2023-07-01 23:38:48 +0900

    fix

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,14 +5,14 @@ import "github.com/gofiber/fiber/v2"
 func CorsMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		if c.Method() == "OPTIONS" {
-			c.Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
 			c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 			c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 			c.Set("Access-Control-Allow-Credentials", "true")
 			return c.SendStatus(fiber.StatusOK)
 		}
 
-		c.Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		c.Set("Access-Control-Allow-Credentials", "true")
