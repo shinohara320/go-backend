639aaa798652881a8e81d590d1f8d86333cbbadb
shinohara320 <daudmanuwu@gmail.com>
2023-08-03 10:50:19 +0900

    adding ssl cert

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,18 +4,16 @@ import "github.com/gofiber/fiber/v2"
 
 func CorsMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		if c.Method() == "OPTIONS" {
-			c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
-			c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-			c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
-			c.Set("Access-Control-Allow-Credentials", "true")
-			return c.SendStatus(fiber.StatusOK)
-		}
+		origin := c.Get("Origin")
 
-		c.Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Set("Access-Control-Allow-Origin", origin)
 		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		c.Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		c.Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Method() == "OPTIONS" {
+			return c.SendStatus(fiber.StatusOK)
+		}
 
 		return c.Next()
 	}
